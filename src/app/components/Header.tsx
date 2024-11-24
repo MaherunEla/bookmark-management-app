c542b27a96d85da4b205caea2b538d4db3bdd68a
Maherun Nessa <meherunela2002@gmail.com>
2024-11-24 19:43:22 +0600

    feat(ui): add button

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,33 @@
+import { BsBookmarksFill } from "react-icons/bs";
+import { MdBookmarkAdd } from "react-icons/md";
+
+const Header = () => {
+  return (
+    <div className=" flex justify-center pt-[100px]">
+      <div className="w-full max-w-[600px] p-5 ">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-white font-bold text-xl p-5">
+            Bookmark Management App
+          </h1>
+          <div className="relative w-full flex items-center justify-between px-10 py-5">
+            <button
+              className="px-4 py-2 border border-pink-800 rounded-lg bg-pink-800 text-white flex items-center justify-center gap-2 hover:bg-pink-700 focus:ring-2 focus:ring-pink-400 focus:outline-none transition-colors duration-200"
+              aria-label="Create new item"
+            >
+              <MdBookmarkAdd size={23} className="text-white" />
+              <span className="text-lg font-semibold">Create</span>
+            </button>
+            <button
+              className="px-4 py-2 border border-teal-800 rounded-lg bg-teal-900 text-white flex items-center justify-center gap-2 hover:bg-teal-700 focus:ring-2 focus:ring-teal-400 focus:outline-none transition-colors duration-200"
+              aria-label="Read Bookmark List"
+            >
+              <BsBookmarksFill size={20} className="text-white" />
+              <span className="text-lg font-semibold">Saved</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,13 @@ import BookmarkCard from "./components/BookmarkCard";
 
 export default function Home() {
   return (
-    <div className="bg-purple-950 w-screen h-screen  flex justify-center pt-[100px]">
-      <div className="w-full max-w-[600px] p-5 ">
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-white font-bold text-xl p-5">
-            Bookmark Management App
-          </h1>
-          <BookmarkCard
-            title="Mongodb"
-            link="https://www.youtube.com/"
-            description=" I have to finish this tutorial"
-            image="/pic1.jpg"
-          />
-        </div>
-      </div>
+    <div className="flex justify-center ">
+      <BookmarkCard
+        title="Mongodb"
+        link="https://www.youtube.com/"
+        description=" I have to finish this tutorial"
+        image="/pic1.jpg"
+      />
     </div>
   );
 }
